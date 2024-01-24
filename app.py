5e7ef54076557fb9afb15fb573500eb348beec61
Leo. de la Silva <lleonardobr@gmail.com>
2024-01-25 04:32:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,22 +21,36 @@ def finalizar_app():
     #os.system('clear') no mac
     print('Finalizando o app....\n')
 
+def opcao_invalida():
+    print('Opcao invalida!\n')
+    input('Digite uma tecla para voltar ao menu')
+    main()
+
+def cadastrar_novo_restaurante():
+    
+
 def escolher_opcoes():
-    opcao_escolhida = int(input('Escolha uma opcao: '))
-    #opcao_escolhida = int(opcao_escolhida)
-    if opcao_escolhida == 1:
-        print('Cadastrar Restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurane')
-    else:
-        finalizar_app()
+    try: 
+        opcao_escolhida = int(input('Escolha uma opcao: '))
+        #opcao_escolhida = int(opcao_escolhida)
+        if opcao_escolhida == 1:
+            print('Cadastrar Restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurane')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcoes()
-    
+
 if __name__ == '__main__':
     main()
