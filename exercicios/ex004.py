1db42f0e3202fb78f5d194826bda288797f1dd39
Leo. de la Silva <lleonardobr@gmail.com>
2024-01-27 12:52:23 +0800

    ....

---
+++ exercicios/ex004.py
@@ -0,0 +1,20 @@
+#Exercícios
+#1 - Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade.
+
+
+
+
+
+
+#2 - Utilizando o dicionário criado no item 1:
+
+#Modifique o valor de um dos itens no dicionário (por exemplo, atualize a idade da pessoa);
+#Adicione um campo de profissão para essa pessoa;
+#Remova um item do dicionário.
+
+#3 - Crie um dicionário utilizando para representar números e seus quadrados de 1 a 5.
+
+#4 - Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário.
+
+#5 - Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionário.
+
