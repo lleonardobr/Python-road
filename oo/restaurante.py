a92646be14451f010882357f19f82e944386b594
Leo. de la Silva <lleonardobr@gmail.com>
2024-02-03 10:30:51 +0800

    Ex002 de O.O

--- oo/restaurante.py
+++ oo/restaurante.py
@@ -5,10 +5,18 @@ class Restaurante:
 
 restaurante_praca = Restaurante()
 restaurante_praca.nome = 'Praca'
-restaurante_praca.categoria = 'Gourmet'
+restaurante_praca.categoria = 'Italiana'
 
 restaurante_pizza = Restaurante()
+restaurante_pizza.nome = 'Pizza Place'
+restaurante_pizza.categoria = 'Fast Food'
+restaurante_pizza.ativo = True
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+print(f'Nome: {restaurante_praca.nome} - Categoria: {restaurante_praca.categoria} - Ativo: {restaurante_praca.ativo}')
+print(f'Nome: {restaurante_pizza.nome} - Categoria:{restaurante_pizza.categoria} - Ativo: {restaurante_pizza.ativo}') 
 
-print(vars(restaurante_praca))
+
+if restaurante_pizza.categoria == 'Fast Food':
+    print('A categoria eh fast food')
+else:
+    print('A categoria nao eh fast food')
